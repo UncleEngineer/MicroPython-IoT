50de6936df22c51af25d13acea8e9c0aef6a7bbc
Uncle Engineer <loong.wissawakorn@gmail.com>
2022-07-27 18:40:04 +0700

    on/off html

---
+++ on-off.py
@@ -0,0 +1,65 @@
+html_on = '''
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>ESP32 - Status</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/pricing/">
+  <link href="https://getbootstrap.com/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="container">
+  <form>
+    <center>
+      <img src="https://raw.githubusercontent.com/UncleEngineer/MicroPython-IoT/main/light-bulb-on.png" width="300">
+     <h3>LED 1</h3>
+          <button  class="btn btn-primary" name="LED" value="ON" type="submit">ON</button>&nbsp;
+        <button  class="btn btn-danger" name="LED" value="OFF" type="submit">OFF</button>
+    </center>
+   </form>
+  </div>
+
+    
+  </body>
+</html>
+
+'''
+
+html_off = '''
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>ESP32 - Status</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/pricing/">
+  <link href="https://getbootstrap.com/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="container">
+  <form>
+    <center>
+      <img src="https://raw.githubusercontent.com/UncleEngineer/MicroPython-IoT/main/light-bulb-off.png" width="300">
+     <h3>LED 1</h3>
+          <button class="btn btn-primary" name="LED" value="ON" type="submit">ON</button>&nbsp;
+        <button class="btn btn-danger" name="LED" value="OFF" type="submit">OFF</button>
+    </center>
+    </form>
+  </div>
+
+    
+  </body>
+</html>
+
+'''
